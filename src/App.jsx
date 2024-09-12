8219a36f86164c91d8438ad71eeb25646d40b58b
Trong Dao <trongdao20402@gmail.com>
2024-09-12 15:59:47 +0700

    useEffect Dependency Array

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,20 @@ import { useState, useEffect } from "react";
 
 export default function App() {
   const [counter, setCounter] = useState(0);
+  const [sync, setSync] = useState(false);
 
   useEffect(() => {
     console.log("Rendering...");
     document.title = "React Tutorial" + counter;
-  });
+  }, [sync]);
 
   return (
     <div>
       <div>You clicked the button {counter} times</div>
       <button onClick={() => setCounter((count) => count + 1)}>Click me</button>
+      <button onClick={() => setSync((currentSync) => !currentSync)}>
+        Sync
+      </button>
     </div>
   );
 }
