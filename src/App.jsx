f170b7b77e32ee458a5e41ab8299e14cb7c0f81d
Trong Dao <trongdao20402@gmail.com>
2024-09-13 16:17:30 +0700

    AbortController

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,12 @@ export default function App() {
   }, [sync]);
 
   useEffect(() => {
+    const controller = new AbortController();
     async function fetchUsers() {
       try {
         const response = await fetch(
-          "https://jsonplaceholder.typicode.com/users"
+          "https://jsonplaceholder.typicode.com/users",
+          { signal: controller.signal }
         );
         const json = await response.json();
         console.log(json);
